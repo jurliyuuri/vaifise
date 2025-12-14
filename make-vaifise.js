23a537b296d84ce101a45db301188fafd880e206
meloviliju <ritterderschwerter178@gmail.com>
2025-12-15 00:15:24 +0900

    upload OTM-JSON file

---
+++ make-vaifise.js
@@ -0,0 +1,33 @@
+import fs from 'fs'
+
+const file = await fs.promises.readFile('./vaifise.json', 'utf8')
+const f = JSON.parse(file) 
+
+const mapped = f.words.map(w => {
+  const newTranslations = w.contents
+    .filter(c => c.title === '訳語')[0]
+    .text.split('\r\n')
+    .filter(t => t.startsWith('【'))
+    .map(t => t.replace(/【/g, '').trim())
+    .map(t => {
+      const [title, ...forms] = t.split('】')
+      return { title, forms }
+    })
+  const newContents = w.contents.filter(c => c.title !== '訳語').concat({
+    title: '備考',
+    text: w.contents
+    .filter(c => c.title === '訳語')[0]
+    .text.split('\r\n')
+    .filter(t => t.startsWith('2'))[0]
+  })
+  return {
+    entry: w.entry,
+    translations: newTranslations,
+    tags: w.tags,
+    contents: newContents,
+    variations: w.variations,
+    relations: w.relations
+  }
+})
+
+fs.promises.writeFile('./vaifise-v2.json', JSON.stringify({ words: mapped }, null, 2), 'utf8')
